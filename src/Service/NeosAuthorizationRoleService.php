d0de512c957b5f467348126e7818c0360167537a
Anna_Paulini <anna.paulini@netlogix.de>
2026-06-15 10:52:44 +0200

    WIP

--- src/Service/NeosAuthorizationRoleService.php
+++ src/Service/NeosAuthorizationRoleService.php
@@ -35,6 +35,7 @@ class NeosAuthorizationRoleService
         'language:read',
         'locale:read',
         'media:read',
+//        'media_thumbnail:read',
         'product:read',
         'product_manufacturer:read',
         'product_media:read',
